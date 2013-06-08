73d9395debd34eb06141a35a0af64d04df32255c
Matthew Scandalis <mscandal@Matthews-MacBook-Pro.local>
2013-06-08 16:31:52 -0400

    added insert and byUrl functions to database. added init

--- database.js
+++ database.js
@@ -1,1 +1,43 @@
-var http = require('http');
+var nano = require('nano')(process.env.CLOUDANT_URL);
+var db = nano.use('credio', function(err) {
+	if(err) {
+		nano.db.create('credio');
+		db = nano.use('credio');
+		var views = {
+			views: {
+				byUrl: (function(doc) {
+					emit(doc.url, doc)
+				}).toString()
+			}
+		}
+		db.insert(views, '_design/doc');
+	}
+})
+
+module.exports.insert = function(options, callback) {
+	var defaults = {
+		type: 'youtube',
+		views: {},
+		postDate: Date.now()
+	}
+	for(i in defaults) {
+		if(!options[i]) {
+			options[i] = defaults[i]
+		}
+	}
+	db.insert(options, function(err, body) {
+		options._id = body.id;
+		options._rev = body.rev;
+		callback(err, options);
+	});
+}
+
+module.exports.byUrl = function(url, callback) {
+	db.view('_design/doc', 'byUrl', { keys: [url] }, function(err, body) {
+		if(err) {
+			callback(err);
+			return
+		}
+		callback(null, body.rows[0].value);
+	})
+}
