50e05d1c6d6a6cd1657ad2fa565574d58beba117
Matthew Scandalis <mscandal@Matthews-MacBook-Pro.local>
2013-06-08 21:30:33 -0400

    testing twitter login

--- auth.js
+++ auth.js
@@ -3,5 +3,9 @@ var database = require('./database');
 module.exports = function(token, tokenSecret, profile, done) {
 	profile.token = token;
 	profile.tokenSecret = tokenSecret;
-	database.insertUser(profile, done);
+	database.insertUser(profile, function(err, user) {
+		console.log('err', err);
+		console.log('user', user);
+		done(err, user);
+	})
 }
